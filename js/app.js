512d76a8e66e28c6b1f8ce6ebae3517dca0afbbf
Andrew Fields <andyfieldsy89@gmail.com>
2018-07-17 09:11:25 -0700

    in class

--- js/app.js
+++ js/app.js
@@ -1,1 +1,28 @@
 // blank template for starting projects
+
+'use strict';
+
+// var turtle = {
+//   legs: 4,
+//   shell: true,
+//   age: 115,
+//   colors: ['turquoise'],
+//   weight: 25,
+//   ninja: true,
+//   snap: function() {
+//     console.log('cowabunga!');
+//   },
+// };
+
+
+var months = ['jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'oct', 'nov', 'dec'];
+var ulEl = document.createElement('ul');
+
+for(var i = 0; i < months.length; i++) {
+  var liEl = document.createElement('li');
+  liEl.textContent = months[i];
+  ulEl.appendChild(liEl);
+}
+
+var monthsSection = document.getElementById('months');
+monthsSection.appendChild(ulEl);
