3873f92bb70e9efe05365097ce612487af37dc81
Andrew Fields <andyfieldsy89@gmail.com>
2018-07-17 10:45:58 -0700

    yes

--- js/app.js
+++ js/app.js
@@ -1,28 +1,60 @@
-// blank template for starting projects
-
 'use strict';
 
-// var turtle = {
-//   legs: 4,
-//   shell: true,
-//   age: 115,
-//   colors: ['turquoise'],
-//   weight: 25,
-//   ninja: true,
-//   snap: function() {
-//     console.log('cowabunga!');
-//   },
-// };
 
 
-var months = ['jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'oct', 'nov', 'dec'];
-var ulEl = document.createElement('ul');
-
-for(var i = 0; i < months.length; i++) {
-  var liEl = document.createElement('li');
-  liEl.textContent = months[i];
-  ulEl.appendChild(liEl);
+function generateRandom(min, max) {
+  return Math.random() * (max - min) + min;
 }
 
-var monthsSection = document.getElementById('months');
-monthsSection.appendChild(ulEl);
+
+var pike = {
+  name: 'First and Pike',
+  minCustomer: 23,
+  maxCustomer:54,
+  avgSale: 3.4,
+  hours: ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm'],
+  customersPerHour: [],
+  cookiesPerHour: [],
+  dailyTotal: 0,
+  calculateCustomers: function() {
+  // for (var i = 0; i < hours.length; i++)
+    for(var hour of pike.hours) {
+      var rando = generateRandom(pike.minCustomer, pike.maxCustomer);
+      pike.customersPerHour.push(rando);
+      for (var numCustomers of pike.customersPerHour) {
+        var cookies = Math.ceil(pike.avgSales + numCustomers);
+        pike.cookiesPerHour.push(cookies);
+        pike.dailyTotal += cookies;
+    }
+    return pike.customersPerHour;
+  }
+
+  calculateSales: function() {
+    pike.calculateCustomers();
+
+    }
+  },
+
+  render: function() {
+    pike.calculateSales();
+    var ulEl = document.createElement('ul');
+    var h2El = document.createElement('h2')
+    h2El.textConent = pike.name;
+    ulEl.appendChild(h2El);
+
+for (var hour in pike.hours) {
+  var liEl = document.createElement('li')
+  liEl.textContent = pike.hours(idx) + ':' + pike.cookiesPerHour[idx] + ' cookies';
+}
+
+
+    var mainEl = document.getElementById('main-content');
+    mainEl.appendChild(ulEl);
+
+  },
+};
+
+var stores = [pike, ];
+for (store of stores) {
+  store.render();
+}
