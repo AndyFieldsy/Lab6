379f7eb1bc180c4cc0194e0141968c9a1d882f08
Andrew Fields <andyfieldsy89@gmail.com>
2018-07-18 09:44:02 -0700

    added TH element to header

--- js/app.js
+++ js/app.js
@@ -49,6 +49,7 @@
 
 var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
 
+var stores = [];
 
 function Store(name, minCustomer, maxCustomer, avgSales, customersPerHour, cookiesPerHour, dailyTotal) {
   this.name = name;
@@ -58,6 +59,8 @@ function Store(name, minCustomer, maxCustomer, avgSales, customersPerHour, cooki
   this.customersPerHour = customersPerHour;
   this.cookersPerHour = cookiesPerHour;
   this.dailyTotal = dailyTotal;
+
+  stores.push(this);
 }
 
 var pike = new Store('First and Pike', 23, 65, 6.3, [], [], 0);
@@ -66,7 +69,6 @@ var seacen = new Store('Seattle Center',11, 38, 3.7, [], [], 0);
 var caphill = new Store('Capitol Hill', 20, 38, 2.3, [], [], 0);
 var alki = new Store('Alki', 2, 16, 4.6, [], [], 0);
 
-var stores = [pike, seatac, seacen, caphill, alki];
 
 Store.prototype.generateRandom = function() {
   return Math.ceil(Math.random() * (this.maxCustomer - this.minCustomer) + this.minCustomer);
@@ -81,6 +83,7 @@ Store.prototype.calculateCustomers = function() {
 Store.prototype.calculateTotal = function() {
   this.dailyTotal = this.customersPerHour.reduce((a,b) => a + b, 0);
 };
+
 
 for (var i = 0; i < hours.length; i++) {
   pike.generateRandom();
@@ -105,11 +108,11 @@ var tdEl2 = document.createElement('td');
 tdEl2.textContent = '';
 trEl2.appendChild(tdEl2);
 for (var l = 0; l < hours.length; l++) {
-  tdEl2 = document.createElement('td');
+  tdEl2 = document.createElement('th');
   tdEl2.textContent = hours[l];
   trEl2.appendChild(tdEl2);
 }
-tdEl2 = document.createElement('td');
+tdEl2 = document.createElement('th');
 tdEl2.textContent = 'Total Customers';
 trEl2.appendChild(tdEl2);
 var semiEl2 = document.getElementById('semi-content');
